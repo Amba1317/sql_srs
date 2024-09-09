9e701ac7dbf428361d1756a675ac8049c4bcb52f
ambassadeur DIALLO <ambassadeur@Lakhoua>
2024-09-09 23:11:58 +0200

    comparaison with real answer

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 # pylint: disable=missing-module-docstring
-
+import logging
+import os
 import duckdb
 import streamlit as st
-import ast
 import pandas as pd
 
 
-#ANSWER_STR = """
-#SELECT * FROM beverages
-#CROSS JOIN food_items
-#"""
+if "data" not in os.listdir():
+    print("creating folder data")
+    logging.error(os.listdir())
+    logging.error("creating folder data")
+    os.mkdir("data")
+
+if "exercises_sql_tables.duckdb" not in os.listdir("data"):
+    exec(open("init_db.py").read())
+    # subprocess.run(["python", "init_db.py"])
 
 # Connexion à la base de données DuckDB
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
@@ -26,43 +31,45 @@ with st.sidebar:
     )
     st.write("You selected:", theme)
 
-    exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
+    exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df().sort_values("last_reviewed").reset_index()
     st.write(exercise)
+
+    exercise_name = exercise.loc[0, "exercise_name"]
+    with open(f"answers/{exercise_name}.sql") as f:
+        answer = f.read()
+
+    solution_df = con.execute(answer).df()
 
 st.header("enter your code:")
 query = st.text_area(label="votre code SQL ici", key="user_input")
 if query:
-   result = con.execute(query).df()
-   st.dataframe(result)
-#
-#    if len(result.columns) != len(
-#        solution_df.columns
-#    ):  # replace with try result = result[solution_df.columns]
-#        st.write("Some columns are missing")
-#    try:
-#        result = result[solution_df.columns]
-#        st.dataframe(result.compare(solution_df))
-#    except KeyError as e:
-#        st.write("Some columns are missing")
-#
-#    n_lines_difference = result.shape[0] - solution_df.shape[0]
-#    if n_lines_difference != 0:
-#        st.write(
-#            f"result has a {n_lines_difference} lines difference with the solution_df"
-#        )
-#
-#
+    result = con.execute(query).df()
+    st.dataframe(result)
+
+    try:
+        result = result[solution_df.columns]
+        st.dataframe(result.compare(solution_df))
+    except KeyError as e:
+        st.write("Some columns are missing")
+
+    n_lines_difference = result.shape[0] - solution_df.shape[0]
+    if n_lines_difference != 0:
+        st.write(
+            f"result has a {n_lines_difference} lines difference with the solution_df"
+    )
+
+
 tab2, tab3 = st.tabs(["Tables", "Solution"])
 
 with tab2:
-    exercise_tables = ast.literal_eval(exercise.loc[0, "tables"])
+    exercise_tables = exercise.loc[0, "tables"]
     for table in exercise_tables:
         st.write(f"table: {table}")
         df_table = con.execute(f"SELECT * FROM {table}").df()
         st.dataframe(df_table)
 
 with tab3:
-   exercise_name = exercise.loc[0, "exercise_name"]
-   with open(f"answers/{exercise_name}.sql") as f:
-       answer = f.read()
+#   exercise_name = exercise.loc[0, "exercise_name"]
+#   with open(f"answers/{exercise_name}.sql") as f:
+#       answer = f.read()
    st.write(answer)
